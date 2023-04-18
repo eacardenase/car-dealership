8ca2e6c25d1e4f6cc3f84969f7b5ead80431cc64
Edwin Cardenas <eacardenase@unal.edu.co>
2023-04-18 18:00:16 -0500

    feat: add cars service

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,9 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { CarsService } from './cars.service';
 
 @Controller('cars')
 export class CarsController {
+  constructor(private readonly carsService: CarsService) {}
+
   @Get()
   getAllCars() {
-    return ['Toyota', 'Honda', 'BMW'];
+    return this.carsService.findAll();
+  }
+
+  @Get(':id')
+  getCarById(@Param('id', ParseIntPipe) id: number) {
+    return this.carsService.findOneById(id);
   }
 }
